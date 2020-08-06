be807d373f9cb9395d59fe5981237a776e882688
bashaadil <adil@shade6.com>
2020-08-06 20:37:05 +0530

    Hosted

--- src/App.js
+++ src/App.js
@@ -1,61 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
-import Message from './Message';
-import db from './firebase';
-
+import Message from "./Message";
+import db from "./firebase";
+import firebase from "firebase";
+import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
+import {IconButton} from "@material-ui/core";
 
 function App() {
+	const [input, setInput] = useState("");
 
-  const [input, setInput] = useState('');
+	const [messages, setMessages] = useState([]);
 
-  const [messages, setMessages] = useState([
-  ]);
+	const [username, setUsername] = useState("");
 
-  const [username, setUsername] = useState('');
-  
-  const sendMessage = (event) => {
-    event.preventDefault();
-    setMessages([...messages, {username: username, text: input}]);
-    setInput('');
-  }
+	const sendMessage = (event) => {
+		event.preventDefault();
+		console.log(username);
+		console.log(input);
+		console.log(messages);
+		db.collection("messages")
+			.add({
+				message: input,
+				username: username,
+				timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+			});
+		setMessages([...messages, { username: username, message: input }]);
+		setInput("");
+	};
 
-  useEffect(() => {
-    setUsername(prompt('Please Enter your name'));
-  }, [])
+	useEffect(() => {
+		setUsername(prompt("Please Enter your name"));
+	}, []);
 
-  useEffect(() => {
-    db.collection('messages').onSnapshot(
-      snapshot => {
-        setMessages(snapshot.docs.map(doc => doc.data()))
-      }
-    )
-  }, [])
+	useEffect(() => {
+		db.collection("messages")
+			.orderBy("timestamp", "desc")
+			.onSnapshot((snapshot) => {
+				setMessages(
+					snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+				);
+			});
+	}, []);
 
+	return (
+		<div className="App">
+			<img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" />
+			<h1>Welcome to Messenger Clone</h1>
+			<h2>Welcome {username}</h2>
+			<form className="app__form">
+				<FormControl className="app__formcontrol">
+					<InputLabel>Enter a Message .....</InputLabel>
+					<Input 
+					className="app__input"
+						value={input}
+						onChange={(event) => setInput(event.target.value)}
+					/>
 
+					<IconButton
+					className= "app__iconbutton"
+						disabled={!input}
+						variant="contained"
+						color="primary"
+						type="submit"
+						onClick={sendMessage}
+					>
+						<SendIcon />
+					</IconButton>
+				</FormControl>
+			</form>
 
-  return (
-    <div className="App">
-      <h1>Welcome to Basha Messenger</h1>
-      <h2>Welcome {username}</h2>
-      <form>
-        <InputLabel>Enter a Message .....</InputLabel>
-        <Input value={input} onChange={event => setInput(event.target.value)} />
-        
-      <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send Messages </Button>
-      </form>
-      {
-        messages.map(m => (
-          <Message username={username} message={m} />
-        ))
-      }
-    </div>
-  );
+			<FlipMove>
+				{messages.map(({ id, message }) => (
+					<Message key={id} message={message} username={username} />
+				))}
+			</FlipMove>
+		</div>
+	);
 }
 
 export default App;
-
-
 
 // <!-- The core Firebase JS SDK is always required and must be listed first -->
 // <script src="/__/firebase/7.17.1/firebase-app.js"></script>
@@ -67,8 +92,6 @@ export default App;
 // <!-- Initialize Firebase -->
 // <script src="/__/firebase/init.js"></script>
 
-
-
 // npm install -g firebase-tools
 
 // firebase login
@@ -76,7 +99,6 @@ export default App;
 //firebase init
 
 //firebase deploy
-
 
 // rules_version = '2';
 // service cloud.firestore {
